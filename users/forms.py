bafd4634ad736bb75205e2be865e4cee9e50d22d
bob <c2064179@newcastle.ac.uk>
2023-11-20 16:45:22 +0000

    Added validate_phone() function to ensure phone number field can only contain digits and dashes in the form of "XXXX-XXX-XXXX"

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
 from wtforms.validators import DataRequired, Email, ValidationError
+import re
 
 def character_check(form, field):
 
@@ -10,11 +11,17 @@ def character_check(form, field):
         if char in excluded_chars:
             raise ValidationError(f"Character {char} is not allowed.")
 
+def validate_phone(self, data_field):
+    p = re.compile("[0-9]{4}[-][0-9]{3}[-][0-9]{4}")
+    if not p.match(data_field.data):
+        raise ValidationError(f"Phone number must be in the form 'XXXX-XXX-XXXX'")
+
+
 class RegisterForm(FlaskForm):
     email = StringField(validators=[DataRequired(), Email()])
     firstname = StringField(validators=[DataRequired(), character_check])
     lastname = StringField(validators=[DataRequired(), character_check])
-    phone = StringField()
+    phone = StringField(validators=[DataRequired(), validate_phone])
     password = PasswordField()
     confirm_password = PasswordField()
     submit = SubmitField()
