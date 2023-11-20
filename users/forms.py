f177e0fd702bafd8b0d66881fb5ab39a409678ef
bob <c2064179@newcastle.ac.uk>
2023-11-20 16:28:49 +0000

    Added character_check() function to filter the characters not allowed in Firstname and Lastname

--- users/forms.py
+++ users/forms.py
@@ -1,12 +1,19 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import DataRequired, Email
+from wtforms.validators import DataRequired, Email, ValidationError
 
+def character_check(form, field):
+
+    excluded_chars = "*?!'^+%&/()=}][{$#@<>"
+
+    for char in field.data:
+        if char in excluded_chars:
+            raise ValidationError(f"Character {char} is not allowed.")
 
 class RegisterForm(FlaskForm):
     email = StringField(validators=[DataRequired(), Email()])
-    firstname = StringField()
-    lastname = StringField()
+    firstname = StringField(validators=[DataRequired(), character_check])
+    lastname = StringField(validators=[DataRequired(), character_check])
     phone = StringField()
     password = PasswordField()
     confirm_password = PasswordField()
